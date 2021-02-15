150148cd519a593083ffb3c55621bc70d2693ee2
unknown <gr.s.joe@gmail.com>
2021-02-15 12:33:44 +0500

    initial project version

---
+++ src/components/vue-form/vue-form.vue
@@ -0,0 +1,26 @@
+<template>
+  <form class="vue-form">
+    <div class="vue-form__reg">
+      <h1 class="vue-form__reg-title">
+        Регистрация
+      </h1>
+      <div class="vue-form__reg-sign">
+        <p class="vue-form__reg-sign-copy">Уже есть аккаунт?</p>
+        <a href="#" class="vue-form__reg-sign-link">Войти</a>
+      </div>
+    </div>
+    <vue-input></vue-input>
+  </form>
+</template>
+
+<script>
+import vueInput from "../vue-input/vue-input.vue";
+export default {
+  components: { vueInput },
+  name: "vue-form",
+};
+</script>
+
+<style lang="less">
+@import url("./vue-form.less");
+</style>
---
+++ src/components/vue-input/vue-input.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="vue-input">
+    <label class="vue-input__label">Имя</label>
+    <input
+      class="vue-input__input"
+      type="text"
+      placeholder="Введите Ваше имя"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "vue-input",
+};
+</script>
+
+<style lang="less">
+@import url("./vue-input.less");
+</style>
